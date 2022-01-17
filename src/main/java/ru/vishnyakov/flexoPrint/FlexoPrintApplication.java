6aaa5aea7de9b36ddeff933ad547fa8e0c1ea0d6
Essensel <Essensel@yandex.ru>
2022-01-17 17:43:51 +0300

    initial commit

---
+++ src/main/java/ru/vishnyakov/flexoPrint/FlexoPrintApplication.java
@@ -0,0 +1,13 @@
+package ru.vishnyakov.flexoPrint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlexoPrintApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlexoPrintApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/vishnyakov/flexoPrint/FlexoPrintApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.vishnyakov.flexoPrint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlexoPrintApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
